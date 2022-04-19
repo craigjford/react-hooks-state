9577276bf2b867b420cfd439c1f53e433ce1c0a5
Craig Ford <craig_ford@icloud.com>
2022-04-18 20:00:49 -0400

    Complete

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,7 +4,9 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
+    console.log('before set state - ', count)
     setCount(count + 1);
+    console.log('after set state - ', count)
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
